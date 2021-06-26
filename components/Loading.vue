006140b189bc7d1d4b523d18bac69d4c4acf38ff
Pedro Felipe <pedrinhof11@gmail.com>
2021-06-26 13:32:27 -0300

    feat: componente de loading

---
+++ components/Loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-overlay :value="active">
+    <v-progress-circular width="3" class="loading" size="80" indeterminate>
+    </v-progress-circular>
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Loading extends Vue {
+  active = false
+
+  start() {
+    this.active = true
+  }
+
+  finish() {
+    setInterval(() => {
+      this.active = false
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  color: $primary;
+}
+</style>
